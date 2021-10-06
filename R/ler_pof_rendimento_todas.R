2c6c63f79ecf1bd4f9a5f6c6415dcdd7102a01e4
Pedro Rubin <prubincosta@gmail.com>
2021-10-05 21:30:11 -0300

    ler_pof_rendimento_todas

--- R/ler_pof_rendimento_todas.R
+++ R/ler_pof_rendimento_todas.R
@@ -1,20 +1,20 @@
-#' Load POF microdata - all expenditure information
+#' Load POF microdata - all income information
 #'
-#' Load POF microdata - expenditure information only
+#' Load POF microdata - income information only
 #' @param microdata_folder The path to the FOLDER that contains the microdata txt files
-#' @return All expenditure information available (as a dataframe)
-#' @examples ler_pof_despesa_todas ("./data_folder");
+#' @return All income information available (as a dataframe)
+#' @examples ler_pof_rendimento_todas ("./data_folder");
 #' @export
 
-ler_pof_despesa_todas <- function(microdata_folder){
+ler_pof_rendimento_todas <- function(microdata_folder){
 
-  lista_pof_despesa <- tibble(num_pof = c(as.character(glue("{microdata_folder}/ALUGUEL_ESTIMADO.txt")),
+  lista_pof_rendimento <- tibble(num_pof = c(as.character(glue("{microdata_folder}/ALUGUEL_ESTIMADO.txt")),
                                           as.character(glue("{microdata_folder}/CADERNETA_COLETIVA.txt")),
                                           as.character(glue("{microdata_folder}/DESPESA_COLETIVA.txt")),
                                           as.character(glue("{microdata_folder}/DESPESA_INDIVIDUAL.txt")),
                                           as.character(glue("{microdata_folder}/OUTROS_RENDIMENTOS.txt")),
                                           as.character(glue("{microdata_folder}/RENDIMENTO_TRABALHO.txt"))))
 
-  pof_desp <- map_dfr(lista_pof_despesa$num_pof,
-                      ler_pof_despesa)
+  pof_desp <- map_dfr(lista_pof_rendimento$num_pof,
+                      ler_pof_rendimento)
 }
