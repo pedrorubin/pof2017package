ead5a3b2fef6887d8d3e2bdb64c147182f9b4be2
Pedro Rubin <prubincosta@gmail.com>
2022-03-11 01:06:23 -0300

    Update montar_tabela_despesa_uc.R

--- R/montar_tabela_despesa_uc.R
+++ R/montar_tabela_despesa_uc.R
@@ -2,16 +2,16 @@ montar_tabela_despesa_uc_one <- function(df_pof_despesa_base,
                                          tipo_despesa = 0,
                                          df_pof_morador_base){
 
-  pof_uc <- get(df_pof_morador_base) %>%
-    mutate(ID_uc = str_c(COD_UPA, NUM_DOM, NUM_UC)) %>%
-    filter(V0306 == "1") %>%
-    # select(ID_uc, UF, RENDA_TOTAL, PESO_FINAL) %>%
-    mutate(across(.fns = as.numeric)) %>%
-    mutate(numero_familias = sum(PESO_FINAL))
+  # pof_uc <- get(df_pof_morador_base) %>%
+  #   mutate(ID_uc = str_c(COD_UPA, NUM_DOM, NUM_UC)) %>%
+  #   filter(V0306 == "1") %>%
+  #   # select(ID_uc, UF, RENDA_TOTAL, PESO_FINAL) %>%
+  #   mutate(across(.fns = as.numeric)) %>%
+  #   mutate(numero_familias = sum(PESO_FINAL))
 
 
-  pof_despesa <- get(df_pof_despesa_base) %>%
-    right_join(pof_uc, by = c("ID_uc", "PESO_FINAL"))
+  pof_despesa <- get(df_pof_despesa_base) #%>%
+    # right_join(pof_uc, by = c("ID_uc", "PESO_FINAL"))
 
   if(tipo_despesa == 0){
 
@@ -20,7 +20,8 @@ montar_tabela_despesa_uc_one <- function(df_pof_despesa_base,
       group_by(ID_uc, PESO_FINAL, Nivel_0) %>%
       summarise(Descricao_0 = unique(Descricao_0),
                 valor_uc = sum(valor,na.rm = T),
-                numero_familias = unique(numero_familias)) %>%
+                # numero_familias = unique(numero_familias)
+                ) %>%
       ungroup() %>%
       mutate(Descricao = str_c(Descricao_0,"_",Nivel_0)) %>%
       select(-c(Nivel_0,Descricao_0)) %>%
@@ -34,7 +35,8 @@ montar_tabela_despesa_uc_one <- function(df_pof_despesa_base,
       group_by(ID_uc, PESO_FINAL, Nivel_1) %>%
       summarise(Descricao_1 = unique(Descricao_1),
                 valor_uc = sum(valor,na.rm = T),
-                numero_familias = unique(numero_familias)) %>%
+                # numero_familias = unique(numero_familias)
+                ) %>%
       ungroup() %>%
       mutate(Descricao = str_c(Descricao_1,"_",Nivel_1)) %>%
       select(-c(Nivel_1,Descricao_1)) %>%
@@ -48,7 +50,8 @@ montar_tabela_despesa_uc_one <- function(df_pof_despesa_base,
       group_by(ID_uc, PESO_FINAL, Nivel_2) %>%
       summarise(Descricao_2 = unique(Descricao_2),
                 valor_uc = sum(valor,na.rm = T),
-                numero_familias = unique(numero_familias)) %>%
+                # numero_familias = unique(numero_familias)
+                ) %>%
       ungroup() %>%
       mutate(Descricao = str_c(Descricao_2,"_",Nivel_2)) %>%
       select(-c(Nivel_2,Descricao_2)) %>%
@@ -62,7 +65,8 @@ montar_tabela_despesa_uc_one <- function(df_pof_despesa_base,
       group_by(ID_uc, PESO_FINAL, Nivel_3) %>%
       summarise(Descricao_3 = unique(Descricao_3),
                 valor_uc = sum(valor,na.rm = T),
-                numero_familias = unique(numero_familias)) %>%
+                # numero_familias = unique(numero_familias)
+                ) %>%
       ungroup() %>%
       mutate(Descricao = str_c(Descricao_3,"_",Nivel_3)) %>%
       select(-c(Nivel_3,Descricao_3)) %>%
@@ -81,7 +85,8 @@ montar_tabela_despesa_uc_one <- function(df_pof_despesa_base,
       group_by(ID_uc, PESO_FINAL, Nivel_4) %>%
       summarise(Descricao_4 = unique(Descricao_4),
                 valor_uc = sum(valor,na.rm = T),
-                numero_familias = unique(numero_familias)) %>%
+                # numero_familias = unique(numero_familias)
+                ) %>%
       ungroup() %>%
       mutate(Descricao = str_c(Descricao_4,"_",Nivel_4)) %>%
       select(-c(Nivel_4,Descricao_4)) %>%
@@ -95,7 +100,8 @@ montar_tabela_despesa_uc_one <- function(df_pof_despesa_base,
       group_by(ID_uc, PESO_FINAL, Nivel_5) %>%
       summarise(Descricao_5 = unique(Descricao_5),
                 valor_uc = sum(valor,na.rm = T),
-                numero_familias = unique(numero_familias)) %>%
+                # numero_familias = unique(numero_familias)
+                ) %>%
       ungroup() %>%
       mutate(Descricao = str_c(Descricao_5,"_",Nivel_5)) %>%
       select(-c(Nivel_5,Descricao_5)) %>%
@@ -124,13 +130,23 @@ montar_tabela_despesa_uc <- function(df_pof_despesa_base,
 
   lista_pof <- list(df_pof_despesa_base = df_pof_despesa_base,
                     tipo_despesa = tipo_despesa,
-                    df_pof_morador_base = df_pof_morador_base)
+                    # df_pof_morador_base = df_pof_morador_base
+                    )
 
   lista_despesa_uc <- pmap(lista_pof,
                            montar_tabela_despesa_uc_one)
 
   df_despesa_uc <- lista_despesa_uc %>%
-    reduce(full_join, by = c("ID_uc", "PESO_FINAL", "numero_familias"))
+    reduce(full_join, by = c("ID_uc", "PESO_FINAL"))
 
-  df_despesa_uc
+  pof_uc <- get(df_pof_morador_base) %>%
+    mutate(ID_uc = str_c(COD_UPA, NUM_DOM, NUM_UC)) %>%
+    filter(V0306 == "1") %>%
+    # select(ID_uc, UF, RENDA_TOTAL, PESO_FINAL) %>%
+    mutate(across(.fns = as.numeric)) %>%
+    mutate(numero_familias = sum(PESO_FINAL)) %>%
+    left_join(df_despesa_uc, by = c("ID_uc", "PESO_FINAL"))
+
+
+
 }
