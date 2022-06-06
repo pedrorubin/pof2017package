c6178362ad00cbc16d9e549e4400322019981b5d
Pedro Rubin <prubincosta@gmail.com>
2022-06-06 14:17:38 -0300

    Update ler_pof_rendimento.R

--- R/ler_pof_rendimento.R
+++ R/ler_pof_rendimento.R
@@ -90,7 +90,7 @@ ler_pof_rendimento <- function(arquivo_microdados){
              NUM_UC = str_pad(NUM_UC, 2, "left", "0"),
              ID_pes = NA,
              across(.cols = c(V9001, V8000_DEFLA,
-                              V9011, FATOR_ANUALIZACAO, PESO_FINAL),
+                              FATOR_ANUALIZACAO, PESO_FINAL),
                     .fns = as.numeric)) %>%
       filter(V9002 >= 7) %>%
       mutate(valor_mensal = (V8000_DEFLA*FATOR_ANUALIZACAO)/12,
